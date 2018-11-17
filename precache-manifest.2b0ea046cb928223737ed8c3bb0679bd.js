6c4ae3a2d230584cd4d4efb4a17c6e2e23d21f30
Felix Roos <roos@entrecode.de>
2018-11-17 23:46:50 +0100

    Updates

---
+++ precache-manifest.2b0ea046cb928223737ed8c3bb0679bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "892f07671933dff4e0f95924373933ea",
+    "url": "/moogwein/static/media/messer.892f0767.png"
+  },
+  {
+    "revision": "aeec0dde9875458d1e55e5aa83813035",
+    "url": "/moogwein/static/media/hang.aeec0dde.jpg"
+  },
+  {
+    "revision": "5d56e6aba4e6529aa994",
+    "url": "/moogwein/static/js/runtime~main.5d56e6ab.js"
+  },
+  {
+    "revision": "616acc8614c704f23fef",
+    "url": "/moogwein/static/js/main.616acc86.chunk.js"
+  },
+  {
+    "revision": "a9d6ba5203de63d53915",
+    "url": "/moogwein/static/js/1.a9d6ba52.chunk.js"
+  },
+  {
+    "revision": "616acc8614c704f23fef",
+    "url": "/moogwein/static/css/main.d8b61a84.chunk.css"
+  },
+  {
+    "revision": "9f294f3f0911930950ec254305dad9af",
+    "url": "/moogwein/index.html"
+  }
+];
