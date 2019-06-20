66e2983aa415c9eeea8c51501d9a68536796db48
Felix Roos <roos@entrecode.de>
2019-06-20 21:40:58 +0200

    fix: prices

--- src/components/Weine.js
+++ src/components/Weine.js
@@ -5,83 +5,88 @@ export const Weine = (props) => (
         <table className="table">
             <thead>
                 <tr>
-                    <th className="align-left" colSpan="2">Weinpreisliste 2018</th>
-                    <th className="align-right"><small>0.75l</small></th>
+                    <th className="align-left" colSpan="2">Preisliste 2018</th>
+                    <th className="align-right"><strong>0.75l</strong></th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>Trabener Kräuterhaus 2016</td>
-                    <td>Riesling - Qualitätswein - trocken</td>
-                    <td className="align-right">6,80€</td>
-                </tr>
-                <tr>
-                    <td>Trarbacher Schloßberg 2016</td>
-                    <td>Riesling - Kabinett - feinherb</td>
-                    <td className="align-right">7,80€</td>
-                </tr>
-                <tr>
-                    <td>Trabener Gaispfad 2016</td>
-                    <td>Riesling - Spätlese</td>
-                    <td className="align-right">12,50€</td>
-                </tr>
-                <tr>
                     <td>Trabener Kräuterhaus 2017</td>
-                    <td>Riesling - Kabinett - trocken</td>
-                    <td className="align-right">7,50€</td>
-                </tr>
-                <tr>
-                    <td>Trarbacher Schloßberg 2017</td>
-                    <td>Riesling - Kabinett - feinherb</td>
-                    <td className="align-right">8,00€</td>
+                    <td>Riesling - Qualitätswein - trocken</td>
+                    <td className="align-right">7,50€ <br/><small>10€/l</small></td>
                 </tr>
                 <tr>
                     <td>Trabener Gaispfad 2017</td>
                     <td>Riesling - Spätlese</td>
-                    <td className="align-right">12,50€</td>
+                    <td className="align-right">12,50€ <br/><small>16,67€/l</small></td>
+                </tr>
+                <tr>
+                    <td>Trabener Kräuterhaus 2018</td>
+                    <td>Riesling - Kabinett - trocken</td>
+                    <td className="align-right">7,50€ <br/><small>10€/l</small></td>
+                </tr>
+                <tr>
+                    <td>Trarbacher Schloßberg 2018</td>
+                    <td>Riesling - Kabinett - feinherb</td>
+                    <td className="align-right">8,00€ <br/><small>10,67€/l</small></td>
+                </tr>
+                <tr>
+                    <td>Trabener Gaispfad 2018</td>
+                    <td>Riesling - Spätlese</td>
+                    <td className="align-right">12,50€ <br/><small>16,67€/l</small></td>
                 </tr>
             </tbody>
             <thead>
                 <tr>
                     <th className="align-left" colSpan="2">Edelbrände <small>aus eigenem Wein-und Obstbau</small></th>
-                    <th className="align-right"><small>0,5l</small></th>
+                    <th className="align-right"><strong>0,5l</strong></th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Rieslingtresterbrand</td>
                     <td>2 Jahre im Eichenholzfass gereift</td>
-                    <td className="align-right">9,50€</td>
+                    <td className="align-right">9,50€ <br/><small>19€/l</small></td>
                 </tr>
                 <tr>
                     <td>Rieslingtresterbrand</td>
                     <td>5 Jahre im Eichenholzfass gereift</td>
-                    <td className="align-right">14,50€</td>
+                    <td className="align-right">14,50€ <br/><small>29€/l</small></td>
+                </tr>
+                <tr>
+                    <td>Rieslingtresterbrand</td>
+                    <td>10 Jahre im Eichenholzfass gereift</td>
+                    <td className="align-right">23,50€ <br/><small>47€/l</small></td>
                 </tr>
                 <tr>
                     <td>Apfelbrand</td>
                     <td>im Eichenholzfass gereift</td>
-                    <td className="align-right">13,50€</td>
+                    <td className="align-right">14,00€ <br/><small>28€/l</small></td>
+                </tr>
+                <tr>
+                    <td>Quitte</td>
+                    <td></td>
+                    <td className="align-right">14,50€ <br/><small>29€/l</small></td>
                 </tr>
                 <tr>
                     <td>Mirabelle</td>
                     <td></td>
-                    <td className="align-right">14,50€</td>
+                    <td className="align-right">14,50€ <br/><small>29€/l</small></td>
                 </tr>
                 <tr>
-                    <td>Weinbergspfirisch</td>
+                    <td>Weinbergspfirsich</td>
                     <td></td>
-                    <td className="align-right">14,50€</td>
+                    <td className="align-right">14,50€ <br/><small>29€/l</small></td>
                 </tr>
                 <tr>
                     <td>Alter Zwetschgenbrand</td>
                     <td>im Eichenholzfass gereift</td>
-                    <td className="align-right">14,50€</td>
+                    <td className="align-right">14,50€ <br/><small>29€/l</small></td>
                 </tr>
                 <tr>
                     <td>Riesling- Weinbrand</td>
                     <td>1991er Trarbacher Taubenhaus, im Eichenholzfass gereift</td>
-                    <td className="align-right">15,00€</td>
+                    <td className="align-right">16,50€ <br/><small>33€/l</small></td>
                 </tr>
             </tbody>
         </table>
